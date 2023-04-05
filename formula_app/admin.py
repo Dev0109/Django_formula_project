f32a2fd24fe279592ff85978537d5e613fd574d2
Dev0109 <devtop0109@gmail.com>
2023-04-06 02:46:10 +0900

    admin

--- formula_app/admin.py
+++ formula_app/admin.py
@@ -1,3 +1,12 @@
+from csv import list_dialects
 from django.contrib import admin
+from .models import Input, Output
 
-# Register your models here.
+class InputAdmin(admin.ModelAdmin):
+    list_display = ('alpha', 'betha', 'phi', 'delta_a', 'delta_p')
+
+class OutputAdmin(admin.ModelAdmin):
+    list_display = ('kag', 'kagh', 'kagv', 'kaph', 'teta_a', 'k0n', 'k0t', 'k0h', 'k0V', 'kpgh', 'kpph', 'kpch', 'teta_p')
+
+admin.site.register(Input, InputAdmin)
+admin.site.register(Output, OutputAdmin)
--- formula_app/models.py
+++ formula_app/models.py
@@ -4,26 +4,26 @@ class Input(models.Model):
     alpha = models.DecimalField(max_digits=19, decimal_places=10)
     betha = models.DecimalField(max_digits=19, decimal_places=10)
     phi = models.DecimalField(max_digits=19, decimal_places=10)
-    deltaa = models.DecimalField(max_digits=19, decimal_places=10)
-    deltap = models.DecimalField(max_digits=19, decimal_places=10)  
+    delta_a = models.DecimalField(max_digits=19, decimal_places=10)
+    delta_p = models.DecimalField(max_digits=19, decimal_places=10)  
 
     def __str__(self):
-        return f"{self.alpha} {self.betha} {self.phi} {self.deltaa} {self.deltap}"
+        return f"{self.alpha} {self.betha} {self.phi} {self.delta_a} {self.delta_p}"
 
 class Output(models.Model):
-    kag = models.ForeignKey("Input", on_delete=models.CASCADE)
-    kagh = models.ForeignKey("Input", on_delete=models.CASCADE)
-    kagv = models.ForeignKey("Input", on_delete=models.CASCADE)
-    kaph = models.ForeignKey("Input", on_delete=models.CASCADE)
-    tetaa = models.ForeignKey("Input", on_delete=models.CASCADE)
-    k0n = models.ForeignKey("Input", on_delete=models.CASCADE)
-    k0t = models.ForeignKey("Input", on_delete=models.CASCADE)
-    k0h = models.ForeignKey("Input", on_delete=models.CASCADE)
-    k0V = models.ForeignKey("Input", on_delete=models.CASCADE)
-    kpgh = models.ForeignKey("Input", on_delete=models.CASCADE)
-    kpph = models.ForeignKey("Input", on_delete=models.CASCADE)
-    kpch = models.ForeignKey("Input", on_delete=models.CASCADE)
-    tetap = models.ForeignKey("Input", on_delete=models.CASCADE)
+    kag = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    kagh = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    kagv = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    kaph = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    teta_a = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    k0n = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    k0t = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    k0h = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    k0V = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    kpgh = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    kpph = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    kpch = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
+    teta_p = models.ForeignKey("Input", on_delete=models.CASCADE, related_name="+")
 
     def __str__(self):
-        return f"{self.kag} {self.kagh} {self.kagv} {self.kaph}{self.tetaa} {self.k0n} {self.k0t}{self.k0h} {self.k0V} {self.kpgh} {self.kpph} {self.kpch} {self.tetap}"
+        return f"{self.kag} {self.kagh} {self.kagv} {self.kaph}{self.teta_a} {self.k0n} {self.k0t}{self.k0h} {self.k0V} {self.kpgh} {self.kpph} {self.kpch} {self.teta_p}"
